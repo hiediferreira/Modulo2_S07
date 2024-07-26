9be4f672553fed358160a1adeecdc2b5b2fb0a54
Hiedi Ferreira <hiedyferreira@gmail.com>
2024-07-26 09:45:15 -0300

    feat(cursos): Método listar todos

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -4,7 +4,6 @@ class CursoController {
     async criar(request, response) {
         try {
             const dados = request.body
-            //duração em horas
 
             if(!dados.nome || !dados.duracao){
                 return response.status(400).json({mensagem: 'Os campos nome e duração são obrigatório!'})
@@ -14,19 +13,21 @@ class CursoController {
             // equivale ao INSERT INTO
 
             console.log(curso)
-            response.status(201).json({mensagem: 'Criado com sucesso!'})
+            response.status(201).json(curso)
 
         } catch(error) {
             response.status(500).json({mensagem: 'Houve um erro ao cadastrar o curso ', error})
         }
     }
+
+    async listarTodos(request, response){
+        try {
+            const cursos = await Curso.findAll()
+            response.json(cursos)
+        } catch(error) {
+            response.status(500).json({mensagem: 'Houve um erro ao listar os dados ', error})
+        }
+    }
 }
-
-
-/*
-Desenvolva uma rota POST com o endpoint ‘/cursos’ capaz de receber, no corpo da requisição, o nome do curso e sua duração em horas. Em seguida, essa rota deve retornar o objeto do curso recém-criado no banco de dados.
-
-Retorne um status code 201 na resposta da requisição. Além disso, valide as informações recebidas no body da requisição. Caso haja algum dado inválido, retorne um status code 400 contendo o motivo do erro.
-*/
 
 module.exports = new CursoController
--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -5,5 +5,6 @@ const CursoController = require('../controllers/CursoController')
 const cursosRoutes = new Router()
 
 cursosRoutes.post('/', CursoController.criar)
+cursosRoutes.get('/', CursoController.listarTodos)
 
 module.exports = cursosRoutes
